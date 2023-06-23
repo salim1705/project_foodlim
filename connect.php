47d30a104eef6bb5b2eff3f33cf87d015d824700
salim1705 <salim.nurikhsan@gmail.com>
2023-06-23 10:42:18 +0700

    komentar

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=localhost;dbname=id20945201_food';
+$user_name = 'id20945201_salim';
+$user_password = 'H421Hwgje{kttQie';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
